bbf35fcc5cbd49f118bc8212488121430a48b3a8
Yue Li <yueli1@stanford.edu>
2017-06-22 16:09:06 -0700

    Added error checks

--- rssfeed.py
+++ rssfeed.py
@@ -1,15 +1,34 @@
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
+from urllib.error import HTTPError
+from urllib.error import URLError
 
 def rssfeed(url):
-	html = urlopen(url)
-	bsObj = BeautifulSoup(html, "html.parser")
-	nameList = bsObj.findAll("author")
+	try:
+ 	    html = urlopen(url)
+	except HTTPError as e:
+   	    print(e)
+	try:
+	    bsObj = BeautifulSoup(html, "html.parser")
+	    nameList = bsObj.findAll("author")
+	except AttributeError as e:
+         return None
 	allNames = list()
 	for name in nameList:
 	    allNames = allNames + (name.getText().split(","))
-	print(allNames)
+	return allNames
+	
 
 sp = rssfeed("https://academic.oup.com/rss/site_5418/3279.xml")
 qje = rssfeed("https://academic.oup.com/rss/site_5504/3365.xml")
 sf = rssfeed("https://academic.oup.com/rss/site_5513/3374.xml")
+
+def printNames(names):
+	if names == None:
+         print("Names could not be found")
+	else:
+   	     print(names)
+
+printNames(sp)
+printNames(qje)
+printNames(sf)
