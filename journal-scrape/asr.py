9bd7d0f6d95e5f11f042d3d97943765426e9f933
Yue Li <yueli1@stanford.edu>
2017-06-23 17:21:50 -0700

    Updated error messages

--- journal-scrape/asr.py
+++ journal-scrape/asr.py
@@ -1,14 +1,26 @@
 import requests
 from bs4 import BeautifulSoup
 import re
+from urllib.error import HTTPError 
+from urllib.error import URLError
 
-page = requests.get('http://journals.sagepub.com/toc/ASR/current').text
-print(page)
-soup = BeautifulSoup(page, "html.parser")
-soup.prettify()
-soupList = soup.findAll("a", class_="entryAuthor", href=re.compile("^(/author/).*(\%2C\+).*$"))
-nameList = set()
-for name in soupList:
-	nameList.add(name.getText())
-for name in nameList:
-	print(name)
+def findAsrAuthors(url):
+	try:
+		page = requests.get(url).text # open the page
+	except HTTPError as e: # print if there's an error opening the page
+		print(e)
+	try:
+		soup = BeautifulSoup(page, "html.parser")
+		# this soupList contains "See all articles..." which has the same tag, so remove it later
+		soupList = soup.findAll("a", class_="entryAuthor", href=re.compile("^(/author/).*(\%2C\+).*$")) 
+	except AttributeError as e:
+		return None
+	nameList = set() # create an empty set of the actual names
+	for name in soupList: # find all the names in the soupList, excluding "See all entries..."
+		if not "articles" in name.get_text(): # remove the "See all articles..."
+			nameList.add(name.getText().lstrip()) # remove the white space in the front
+	for name in nameList: # print the names
+		print(name)
+	return nameList # return the names
+
+asr = findAsrAuthors("http://journals.sagepub.com/toc/ASR/current")
