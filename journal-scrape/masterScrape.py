32b1e46e0acfb6374e37e894e015fca71789c55b
Yue Li <yueli1@stanford.edu>
2017-06-27 14:37:23 -0700

    Updated with csb

--- journal-scrape/masterScrape.py
+++ journal-scrape/masterScrape.py
@@ -4,6 +4,50 @@ from urllib.error import HTTPError
 from urllib.error import URLError
 from urllib.request import urlopen 
 import re
+import csv
+
+CPINAMES = "http://inequality.stanford.edu/_affiliates.csv"
+JPAMURL = "http://onlinelibrary.wiley.com/journal/10.1002/(ISSN)1520-6688"
+JOMFURL = "http://onlinelibrary.wiley.com/journal/10.1111/(ISSN)1741-3737"
+WILEYURL = "http://onlinelibrary.wiley.com/"
+OUPURL = "http://academic.oup.com/"
+DEMOGRAPHYURL = "https://link.springer.com/search?sortOrder=newestFirst&facet-content-type=Article&facet-journal-id=13524"
+NBERURL = "http://www.nber.org/new.html"
+APSRURL = "https://www.cambridge.org/core/journals/american-political-science-review"
+ASJURL = "http://www.journals.uchicago.edu/toc/ajs/current"
+ASRURL = "http://journals.sagepub.com/toc/ASR/current"
+
+
+def firstLast(content):
+	return " ".join(content)
+
+def lastFirst(content):
+	return ", ".join(content[::-1])
+
+def firstInitialLast(content):
+	content[0] = content[0][:1]
+	return ", ".join(content[::-1])
+
+def lastFirstInitial(content):
+	return " ".join(content)
+
+def getAffiliateNames(url):
+	html = urlopen(url)
+	cr = csv.reader(html.read().decode('utf-8').splitlines())
+	included_cols = [0, 1]
+	nameDict = {}
+
+	for row in cr:
+		content = list(row[i] for i in included_cols)
+		name = [firstLast(content), lastFirst(content), firstInitialLast(content), lastFirstInitial(content)]
+		nameDict[firstLast(content)] = name
+
+	for key in nameDict:
+		for name in nameDict[key]:
+			print(name)
+	return nameDict
+
+
 
 def openUrlRequests(url):
 	try:
@@ -13,18 +57,25 @@ def openUrlRequests(url):
    	    print(e)
    	    return None
 
+def removeMiddleName(allNames):
+	nameList = list()
+	for name in allNames: 
+		fml = name.split(" ") # obtain first middle last name
+		fm = [fml[0], fml[-1]]
+		nameList.append(" ".join(fm))
+	return(nameList)
 
 def findAsjAuthors(url):
      html = openUrlRequests(url)
      try:
      	bsObj = BeautifulSoup(html, "html.parser")
-     	allNames = bsObj.findAll("span", {"class":"hlFld-ContribAuthor"})
+     	allNamesUnclean = bsObj.findAll("span", {"class":"hlFld-ContribAuthor"})
+     	allNames = list()
+     	for name in allNamesUnclean:
+     		allNames.append(name.get_text())
+     	return removeMiddleName(allNames)
      except AttributeError as e: # return if there is an attribute error
      	return None
-     nameList = list()
-     for name in allNames:
-     	nameList.append(name.get_text())
-     return nameList
 
 def findApsrAuthors(url):
 	html = openUrlRequests(url)
@@ -36,7 +87,7 @@ def findApsrAuthors(url):
 		for name in nameList:
 			name = name.get_text().lstrip("\n").title()
 			allNames = allNames + pattern.split(name)
-		return allNames
+		return removeMiddleName(allNames)
 	except AttributeError as e: # return if there is an attribute error
          return None
 
@@ -49,10 +100,10 @@ def findAsrAuthors(url):
 		soupList = soup.findAll("a", class_="entryAuthor", href=re.compile("^(/author/).*(\%2C\+).*$")) 
 	except AttributeError as e:
 		return None
-	nameList = set() # create an empty set of the actual names
+	nameList = list() # create an empty set of the actual names
 	for name in soupList: # find all the names in the soupList, excluding "See all entries..."
 		if not "articles" in name.get_text(): # remove the "See all articles..."
-			nameList.add(name.getText().lstrip()) # remove the white space in the front
+			nameList.append(name.getText().lstrip()) # remove the white space in the front
 	return nameList # return the names
 
 
@@ -90,11 +141,16 @@ def rssFeed(url):
 	for name in nameList: # loop through all the names
 		name = name.get_text().rstrip() # get text and the white space at the end
 		allNames = allNames + pattern.split(name) # split at the given patterns
-	return allNames # return all names
+	finalName = list() # problem: some names have period after them, some names have more than one initial
+	for name in allNames:
+		uncleanNames = name.split(" ") # split names with spaces
+		uncleanNames[1] = uncleanNames[1][:1] # for the first name (in the second index), get only the first leter
+		finalName.append(" ".join(uncleanNames)) # join the names back together
+	return finalName # return all names
 
 def openRss(journal): # open into the rss feed
 	try:
-		html = urlopen("https://academic.oup.com/" + journal) # open webpage and read
+		html = urlopen(OUPURL + journal) # open webpage and read
 	except HTTPError as e: # print error if it encounters any
    	    print(e)
 	try:
@@ -151,15 +207,16 @@ def openCurrentWiley(url): # navigate to the current issue
      try:
           soup = BeautifulSoup(html, "html.parser")
           current = soup.find("a", {"id":"currentIssueLink"}) # find the link to the current issue
-          currentUrl = "http://onlinelibrary.wiley.com/" + current.attrs['href'] # get to the current issue
+          currentUrl = WILEYURL + current.attrs['href'] # get to the current issue
      except AttributeError as e: # return if there is an attribute error
           return None
      return findWileyAuthors(currentUrl)
 
+cpiAffliates = getAffiliateNames(CPINAMES)
 
-jpam = openCurrentWiley("http://onlinelibrary.wiley.com/journal/10.1002/(ISSN)1520-6688") # journal 
+jpam = openCurrentWiley(JPAMURL) # journal of policy analysis and management
 
-jomf = openCurrentWiley("http://onlinelibrary.wiley.com/journal/10.1111/(ISSN)1741-3737") # journal of marriage and family
+jomf = openCurrentWiley(JOMFURL) # journal of marriage and family
 
 sp = openRss("sp") # social politics 
 
@@ -167,15 +224,16 @@ sf = openRss("sf") # social force
 
 qje = openRss("qje") # quarterly journal of economics
 
-demography = findDemographyAuthors("https://link.springer.com/search?sortOrder=newestFirst&facet-content-type=Article&facet-journal-id=13524")
+demography = findDemographyAuthors(DEMOGRAPHYURL) # demography
 
-nber = findNberAuthors("http://www.nber.org/new.html")
+nber = findNberAuthors(NBERURL) # nber
 
-apsr = findApsrAuthors("https://www.cambridge.org/core/journals/american-political-science-review")
+apsr = findApsrAuthors(APSRURL) # american political science review
 
-asj = findAsjAuthors("http://www.journals.uchicago.edu/toc/ajs/current")
+asj = findAsjAuthors(ASJURL) # american sociology journal
 
-asr = findAsrAuthors("http://journals.sagepub.com/toc/ASR/current")
+asr = findAsrAuthors(ASRURL) # american sociological review
+
 
 allAuthors = {"Journal of Policy and Analysis" : jpam,
 			  "Journal of Marriage and Family" : jomf,
