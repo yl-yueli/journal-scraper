d54f0bd2fc37b5b68084544fb4c09631a268d81f
Yue Li <yueli1@stanford.edu>
2017-06-28 15:21:18 -0700

    Print all necessary information

--- journal-scrape/apsr.py
+++ journal-scrape/apsr.py
@@ -4,6 +4,14 @@ from urllib.error import HTTPError
 from urllib.error import URLError
 import re
 
+def removeMiddleName(allNames): # remove middle names (takes the first word and the last word)
+	nameList = list()
+	for name in allNames: 
+		fml = name.split(" ") # obtain first middle last name
+		fm = [fml[0], fml[-1]] # create a list with the first word and the last word (assume first and last name)
+		nameList.append(" ".join(fm)) # join together the first and last name as a string, add to the list of names
+	return(nameList)
+
 def findApsrAuthors(url):
 	try:
 		html = requests.get(url).text 
@@ -11,18 +19,31 @@ def findApsrAuthors(url):
 		print(e)
 	try:
 		soup = BeautifulSoup(html, "html.parser")
-		nameList = soup.findAll("li", {"class":"author"})
-		allNames = list()
-		pattern = re.compile("\s*,\s*|\s+$")
-		for name in nameList:
-			name = name.get_text().lstrip("\n")
-			allNames = allNames + pattern.split(name)
-		for name in allNames:
-			print(name)
-		#print(allNames)
-		return allNames
+		allInfo = soup.findAll("ul", {"class":"details"})
+		allAuthorInfo = {}
+		for info in allInfo:
+			title = info.find("li", {"class":"title"}).get_text()
+			date = info.find("span", {"class":"date"}).get_text()
+			link = info.find("a", {"class":"url"}).get_text()
+			authors = info.find("li", {"class":"author"}).get_text().lstrip("\n").title()
+			authorInfo = "Title: " + title + "\n\tDate: " + date + "\n\tLink: " + link + "\n\tAuthor(s)" + authors + "\n"
+			pattern = re.compile("\s*,\s*|\s+$") # remove commas and spaces at the end
+			eachName = removeMiddleName(pattern.split(authors))
+			for name in eachName:
+				allAuthorInfo[name] = authorInfo
+		return allAuthorInfo
+
 	except AttributeError as e: # return if there is an attribute error
          return None
 
+def printNames(names): # simple printing command for testing
+	if names == None:
+         print("Names could not be found")
+	else:
+		for name in names:
+			print(name + "\n" + names[name])
+
 # automatic link to current issue
-apsr = findApsrAuthors("https://www.cambridge.org/core/journals/american-political-science-review")
+apsr = findApsrAuthors("https://www.cambridge.org/core/journals/american-political-science-review/latest-issue")
+printNames(apsr)
+	
